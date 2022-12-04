4b8dbc170a5ab11001e45a37b7d651ff8ef13762
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-12-04 23:29:42 +0900

    ユーザー情報編集機能の実装
    ・users/registrationコントローラーの実装
    ・パスワード以外のユーザー情報の編集の場合はパスワードが不要

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.new
+    @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
-  def update
-    if @user.update(user_params)
-      redirect_to user_path(current_user.id)
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   if @user.update
+  #     redirect_to user_path(current_user.id)
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   private
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
   root to: "outputs#index"
   resources :outputs
   resources :users, only: [:show, :edit, :update, :destroy]
