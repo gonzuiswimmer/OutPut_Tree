cfdd787690ac494c9d5b0c4bed7851cafd0945d0
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-12-05 18:22:16 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_output
+  before_action :set_item, only: [:create]
 
   def new
     @item = Item.new
@@ -11,12 +12,25 @@ class ItemsController < ApplicationController
   def edit
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
   private
   def set_output
     @output = Output.find(params[:output_id])
   end
 
   def item_params
-    params.require(:item).permit(:item_name, :item_detail, :price).merge(user_id: current_user.id, output_id: params[:id])
+    params.require(:item).permit(:item_name, :item_detail, :price).merge(user_id: current_user.id, output_id: @output.id)
+  end
+
+  def set_item
+    @item = Item.new
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
 
+  validates :item_name, presence: true
+  validates :item_detail, presence: true
+  validates :price, presence: true
+
   belongs_to :output
   belongs_to :user
 end
