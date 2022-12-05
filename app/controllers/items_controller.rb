7e819099665003d5438867a25092afa1098056a3
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-12-05 16:58:43 +0900

    商品出品ページの実装
    出品ページの作成、itemsコントローラーの追記、ルーティングの修正、出品ページへのURLリンクの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :set_output
+
   def new
+    @item = Item.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def set_output
+    @output = Output.find(params[:output_id])
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_detail, :price).merge(user_id: current_user.id, output_id: params[:id])
   end
 end
