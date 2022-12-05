368c900b8c6d7380991510e08fb8bef2f397ca66
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-12-05 15:20:00 +0900

    アウトプット編集機能の実装

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -19,6 +19,19 @@ class OutputsController < ApplicationController
     end
   end
 
+  def edit
+    @output = Output.find(params[:id])
+  end
+
+  def update
+    @output = Output.find(params[:id])
+    if @output.update(output_params)
+      redirect_to root_path
+    else 
+      render 'edit'
+    end 
+  end
+
   private
   def output_params
     params.require(:output).permit(:title, :detail, :post_date, :genre_id).merge(user_id: current_user.id)
