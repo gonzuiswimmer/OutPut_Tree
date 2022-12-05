0ded1c96cb66257b14307477ae1d8cdc20d63fef
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-12-05 12:09:56 +0900

    アウトプット投稿機能の実装（途中）
    バリデーションの追加、createアクションの追記、form項目の追記

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -5,4 +5,19 @@ class OutputsController < ApplicationController
   def new
     @output = Output.new
   end
+
+  def create
+    @output = Output.new(output_params)
+    if @output.valid?
+      @output.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def output_params
+    params.require(:output).permit(:title, :detail, :post_date).merge(user_id: current_user.id)
+  end
 end
--- app/models/output.rb
+++ app/models/output.rb
@@ -1,2 +1,6 @@
 class Output < ApplicationRecord
+  validates :title, presence: true
+  validates :detail, presence: true
+  validates :post_date, presence: true
+  validates :genre_id, presence: true
 end
